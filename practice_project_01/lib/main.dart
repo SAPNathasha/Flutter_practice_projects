ac4c46fa247b49b339d63413d398e5e0f887e603
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-05-03 18:52:11 +0530

    Set up clearly the app

--- practice_project_01/lib/main.dart
+++ practice_project_01/lib/main.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first app")),
-        body: Column(
-          children: [
-            Text("The question!"),
-            ElevatedButton(child: Text("Answer 1"), onPressed: null,),
-            ElevatedButton(child: Text("Answer 2"), onPressed: null,),
-            ElevatedButton(child: Text("Answer 3"), onPressed: null,)
-          ],
-        )
-        ),
-    
-    );
+    return MaterialApp(home: Text("Hello!"));
   }
 }
-
